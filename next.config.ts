9b11ee2452986bd43a1c0184cf2312a1c5a3ae46
Yang Seungpil <ysplab@gmail.com>
2025-04-23 17:03:43 +0900

    Fix merge conflict in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // static export 사용 시 필요한 설정
 };
 
 export default nextConfig;
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-}
-
-module.exports = nextConfig
->>>>>>> c425896ebb6aa5cac1e78226dba0c5f612d00537
